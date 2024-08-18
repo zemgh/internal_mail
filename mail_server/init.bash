7578f0f60a890ff852b3c68fb5ed7217da62e62c
zemgh <bast3ran@yandex.ru>
2024-08-18 18:58:25 +0300

    minor updates

--- mail_server/init.bash
+++ mail_server/init.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python manage.py migrate
-printf "\nMigrates completed.\n"
-
-printf  "\nEnter admin and service password:\n"
+echo "Enter admin and service password: "
 read -s password
+
+python manage.py migrate
+echo "Migrates completed."
 
 python manage.py shell -c "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser(username='admin', password='$password')"
 python manage.py shell -c "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser(username='service', password='$password')"
 
-printf "\nServer ready.\n\n"
+echo "Server ready."
 
